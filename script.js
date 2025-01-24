0431f8c987a0b31798a53421a4649b8d8c48d69c
MercyPalleti <palletimercy1@gmail.com>
2025-01-24 15:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const makeChange = (c) => {
   // your name here
+	q=0;
+	d=0;
+	n=0;
+	p=0;
+	while(c>0){
+		if(c>=25){
+			q=Math.floor(c/25);
+			c=c%25;
+		}
+		if(c>=10){
+			d=Math.floor(c/10);
+			c=c%10;
+		}
+		if(c>=5){
+			n=Math.floor(c/5);
+			c=c%5;
+		}
+		p=c;
+		obj={"q":q,
+			"d":d,
+			"n":n,
+			"p":p};
+		return obj;
+	}
 };
 
 // Do not the change the code below
